0960403b13154896af065dce36e86f9a45520b34
BillManners <manners.w@etoncollege.org.uk>
2019-11-26 11:05:18 +0000

    God what is wrong with this

--- Tamagotchi/Tamagotchi/tamagotchiClass.swift
+++ Tamagotchi/Tamagotchi/tamagotchiClass.swift
@@ -8,14 +8,35 @@
 
 import Foundation
 
-class tamagotchi {
+class Tamagotchi {
     var age: Int = 0
     var weight: Int = 5
-    var satisfaction: Int = 0
+    var satisfaction: Int = 3
     var awake: Bool = false
     var alive: Bool = true
     var droppings: Bool = false
     var hungry: Bool = false
     var tired: Bool = false
-    
+//    func playGame (satisfaction : Int, tired : Bool, hungry : Bool) -> (Int){
+//        if tired == false && hungry == false{
+//            self.satisfaction += 1
+//        }
+//        else{
+//            print(self.satisfaction)
+//            self.satisfaction = self.satisfaction - 2
+//        }
+//
+//        if Int.random(in: 1..<10) > 5{
+//            self.hungry = true
+//            print ("Your tamagotchi is hungry after a fun game")
+//        }
+//        if Int.random(in: 1..<10) > 5{
+//            self.tired = true
+//            print ("Your tamagotchi is tired after a fun game")
+//        }
+//        if self.satisfaction < 0{
+//            let alive = false
+//        }
+//        return (self.satisfaction)
+//    }
 }
--- Tamagotchi/Tamagotchi_tests/Tamagotchi_tests.swift
+++ Tamagotchi/Tamagotchi_tests/Tamagotchi_tests.swift
@@ -8,39 +8,55 @@
 
 import XCTest
 
-class Tamagotchi_tests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+//class Tamagotchi_tests: XCTestCase {
+//
+//    override func setUp() {
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//
+//    override func tearDown() {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
     
     func testInitInsertsDefaultValues() {
         //arrange
-        var Tamagotchi = tamagotchi()
+        var tamagotchi = Tamagotchi()
         //act
-        let test = Tamagotchi.age
-        let test2 = Tamagotchi.weight
+        let test = tamagotchi.age
+        let test2 = tamagotchi.weight
         //assert
         XCTAssertEqual(test, 0)
         XCTAssertEqual(test2, 5)
     }
-    s
     
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
-}
+//    func testPlayGameWorksWhenTamagotchiIsTiredAndNotTired () {
+//        //arrange
+//        var tamagotchi = Tamagotchi()
+//        var satisfaction : Int = 3
+//        var tired1 : Bool = true
+//        var tired2 : Bool = false
+//        var hungry : Bool = false
+//        //act
+//        let answerSatisfaction1 = tamagotchi.playGame(satisfaction : satisfaction, tired : tired1, hungry : hungry)
+//        let answerSatisfaction2 = tamagotchi.playGame(satisfaction : satisfaction, tired : tired2, hungry : hungry)
+//        //assert
+//        XCTAssertEqual(answerSatisfaction1, 1)
+//        XCTAssertEqual(answerSatisfaction2, 4)
+        
+        
+//    }
+//    
+//
+//    func testExample() {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
+//
+//}
